c98e87d15b73a7204199863bc36f8ce66d49a55c
melanirodriguez1 <melani.d.rodriguez@outlook.com>
2021-11-24 21:06:52 -0300

    agregado llenar cafetera

--- cafe.py
+++ cafe.py
@@ -1,4 +1,8 @@
 class Cafetera:
-    def __init__(self,capacidad):
+    def __init__(self,):
         self.__capacidad_maxima = 1000
+        self.__capacidad_actual = 0
+    
+    def llenar_cafetera(self,capacidad):
         self.__capacidad_actual = capacidad
+        
